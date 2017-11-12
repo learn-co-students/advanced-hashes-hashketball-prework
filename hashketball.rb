45ce37afa5fcd21e64cbf1cc644d601d1802e509
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-12 20:24:11 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -108,3 +108,16 @@ def player_stats(player)
     end
   end
 end
+
+def big_shoe_rebounds
+  winner_chicken_dinner = [0, 0]
+  game_hash.each_value do |team|
+    team[:players].each do |name, player_data|
+      if player_data[:shoe] > winner_chicken_dinner[0]
+        winner_chicken_dinner[0] = player_data[:shoe]
+        winner_chicken_dinner[1] = player_data[:rebounds]
+      end
+    end
+  end
+  return winner_chicken_dinner[1]
+end
