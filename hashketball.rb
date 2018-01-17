37b9cec2a90996f865947818ac3b7b739c61fee8
Caroline Lee <carolinel2610@gmail.com>
2018-01-17 02:57:34 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,93 @@
 # Write your code here!
+require "pry"
+
+def game_hash
+  {home: {team_name: "Brooklyn Nets", colors: ["Black","White"],
+      players: {
+        "Alan Anderson" => {
+          number: 0, shoe: 16, points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks: 1 },
+        "Reggie Evans" => {
+          number: 30, shoe: 14, points: 12, rebounds: 12, assists: 12, steals: 12, blocks: 12, slam_dunks: 7} ,
+        "Brook Lopez" => {
+          number: 11, shoe: 17, points: 17, rebounds: 19, assists: 10, steals: 3, blocks: 1, slam_dunks: 15} ,
+        "Mason Plumlee" => {
+          number: 1, shoe: 19, points: 26, rebounds: 12, assists: 6, steals: 3, blocks: 8, slam_dunks: 8} ,
+        "Jason Terry" => {
+          number: 31, shoe: 15, points: 19, rebounds: 2, assists: 2, steals: 4, blocks: 11, slam_dunks: 1}
+      }
+    },
+    away: {team_name: "Charlotte Hornets", colors: ["Turquoise","Purple"],
+      players: {
+        "Jeff Adrien" => {
+          number: 4, shoe: 18, points: 10, rebounds: 1, assists: 1, steals: 2, blocks: 7, slam_dunks: 2} ,
+        "Bismak Biyombo" => {
+          number: 0, shoe: 16, points: 12, rebounds: 4, assists: 7, steals: 7, blocks: 15, slam_dunks: 10} ,
+        "DeSagna Diop" => {
+          number: 2, shoe: 14, points: 24, rebounds: 12, assists: 12, steals: 1, blocks: 1, slam_dunks: 5} ,
+        "Ben Gordon" => {
+          number: 8, shoe: 15, points: 33, rebounds: 3, assists: 2, steals: 22, blocks: 5, slam_dunks: 0} ,
+        "Brendan Haywood" => {
+          number: 33, shoe: 15, points: 6, rebounds: 12, assists: 12, steals: 4, blocks: 11, slam_dunks: 12}
+      }
+    }
+  }
+end
 
 
+def num_points_scored(player_name)
+  game_hash.each{|team, team_info|
+    team_info[:players].each{|player,info|
+      if player == player_name
+        return info[:points]
+      end
+    }
+  }
+end
 
+def shoe_size(player_name)
+  game_hash.each{|team, team_info|
+    team_info[:players].each{|player,info|
+      if player == player_name
+        return info[:shoe]
+      end
+    }
+  }
+end
 
+def team_colors (team_name)
+  game_hash.each{|team, team_info|
+    if team_info[:team_name] == team_name
+      return team_info[:colors]
+    end
+  }
+end
 
+def team_names
+  array = []
+  game_hash.each{|team, team_info|
+    array << team_info[:team_name]
+  }
+  array
+end
 
+def player_numbers(team_name)
+  array = []
+  game_hash.each{|team, team_info|
+    if team_info[:team_name] == team_name
+      team_info[:players].values.each{|x|
+        array << x[:number]
+      }
+    end
+  }
+  array
+   #returns the player jersey numbers (FAILED - 5)
+   #returns an array of the jersey number
+end
 
+def player_stats
+   #returns all stats for a given player (FAILED - 6)
+end
 
-
-
+def big_shoe_rebounds
+   #returns the number of rebounds of the player with the biggest shoe size (FAILED - 7)
+end
