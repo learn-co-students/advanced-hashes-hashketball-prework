d51ae302e93739793ff068bc730da2d124cb3a0a
Matthew Garrett <mdgarrett1984@gmail.com>
2018-01-21 23:24:35 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,15 @@
 # Write your code here!
-
-
-
-
-
-
-
-
-
-
+def game_hash
+  {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: "15"
+    },
+    away: {
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: "20"
+    }
+  }
+end
