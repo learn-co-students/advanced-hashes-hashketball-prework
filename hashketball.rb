be1fabc05794477b083301e937a358b5a0880a70
github username <github email address>
2018-01-29 20:08:01 -0500

    first day code review

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,4 @@
-require "pry"
+require 'byebug'
 
 def game_hash
   {
@@ -133,39 +133,34 @@ end
 
 def num_points_scored(name)
   game_hash.each do |location, team_data|
-      team_data.each do |attribute, data|
-          if data.is_a? Hash
-                data.each do |player,stats|
-                  if player == name
-                    return game_hash[location][attribute][name][:points]
-                  end
-                end
-          end
+
+    #Going to :players and staying in nested hash
+    team_data[:players].each do |player , stats|
+      if player == name
+        #Using stats instead of writing the whole key to get to the value
+        return stats[:points]
       end
+    end
   end
 end
 
 def shoe_size(name)
   game_hash.each do |location, team_data|
-      team_data.each do |attribute, data|
-          if data.is_a? Hash
-                data.each do |player,stats|
-                  if player == name
-                    return game_hash[location][attribute][name][:shoe]
-                  end
-                end
-          end
+    #Going to :players and skipping other hashes
+    team_data[:players].each do |player , stats|
+      if player == name
+        #Using stats instead of writing the whole key to get to the valuw
+        return stats[:shoe]
       end
+    end
   end
 end
 
 def team_colors(team)
   game_hash.each do |location, team_data|
-      team_data.each do |attribute,data|
-        if data == team
-          return game_hash[location][:colors]
-        end
-      end
+    if team_data[:team_name] == team
+      return team_data[:colors]
+    end
   end
 end
 
@@ -176,31 +171,29 @@ def team_names
 end
 
 def player_numbers(team)
-  numbers_array = []
-  game_hash.each do |location, team_data|
-    if game_hash[location][:team_name] == team
-      team_data.each do |attribute, data|
-        if data.is_a? Hash
-          data.each do |player,stats|
-            numbers_array << game_hash[location][attribute][player][:number]
-          end
-        end
-      end
-    end
+  #see if team matches
+  right_team = check_team(team)
+
+  #then grab hash of players
+  players = right_team[:players]
+
+  #map player numbers
+  players.map do |name, stats|
+    stats[:number]
   end
-  numbers_array
+end
+
+def check_team(team)
+  teams_array = game_hash.values
+  teams_array.find do |team_data|
+    team_data[:team_name] == team
+  end
 end
 
 def player_stats(name)
   game_hash.each do |location, team_data|
-    team_data.each do |attribute, data|
-      if data.is_a? Hash
-        data.each do |player,stats|
-          if player == name
-            return game_hash[location][attribute][player]
-          end
-        end
-      end
+    if team_data[:players][name]
+      return team_data[:players][name]
     end
   end
 end
@@ -209,18 +202,101 @@ def big_shoe_rebounds
   shoe_size = 0
   shoe_player_rebounds = nil
   game_hash.each do |location, team_data|
-    team_data.each do |attribute, data|
-      if data.is_a? Hash
-        data.each do |player,stats|
-          stats.each do |stat,value|
-            if game_hash[location][attribute][player][:shoe] > shoe_size
-              shoe_size = game_hash[location][attribute][player][:shoe]
-              shoe_player_rebounds = game_hash[location][attribute][player][:rebounds]
-            end
-          end
-        end
+    team_data[:players].each do |player, stats|
+      if stats[:shoe] > shoe_size
+        shoe_size = stats[:shoe]
+        shoe_player_rebounds = stats[:rebounds]
       end
     end
   end
   shoe_player_rebounds
+end
+
+def most_points_scored
+  #list of player names and their points
+
+  #take list and compare each player one by one
+  points = 0
+  player_with_most_points = nil
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |player, stats|
+      if stats[:points] > points
+        points = stats[:points]
+        player_with_most_points = player
+      end
+    end
+  end
+  player_with_most_points
+end
+
+def winning_team
+  #get one team's list of Points
+  home_point_list = team_points(:home)
+  #add them
+  home_points = add_points(home_point_list)
+  #get another team's list of Points
+  away_point_list = team_points(:away)
+  #add them
+  away_points = add_points(away_point_list)
+  #return whoever is larger
+  if home_points > away_points
+    return game_hash[:home][:team_name]
+  elsif away_points > home_point_list
+    return game_hash[:away][:team_name]
+  end
+end
+
+def team_points(home_or_away_team)
+  team_data = game_hash[home_or_away_team]
+  team_data[:players].map do |player, stats|
+    stats[:points]
+  end
+end
+
+def add_points(team_points)
+  total = 0
+  team_points.each do |point|
+    total += point
+  end
+  total
+end
+
+def player_with_longest_name
+  #create list of player names
+  player_list
+  #set longest name
+  longest_name = ""
+  #go through each player and check size
+  player_list.each do |player|
+    if longest_name.size < player.size
+      longest_name = player
+    end
+  end
+  longest_name
+end
+
+def player_list
+  list = game_hash[:home][:players].keys << game_hash[:away][:players].keys
+  list.flatten
+end
+
+def long_name_steals_a_ton?
+  most_steals == player_with_longest_name
+end
+
+def most_steals
+  #list of player names and their points
+
+  #take list and compare each player one by one
+  steals = 0
+  player_with_steals = nil
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |player, stats|
+      if stats[:steals] > steals
+        steals = stats[:steals]
+        player_with_steals = player
+      end
+    end
+  end
+  player_with_steals
 end
