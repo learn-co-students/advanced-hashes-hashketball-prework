38b26fefbc10dff9e113ee29ac5aa15c1fe404af
whynotski <laurawynosky@gmail.com>
2018-06-21 15:47:08 -0400

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -103,4 +103,18 @@ require 'pry'
               :slam_dunks => "12"}}
             },
         }
+
+def home_team_name
+    game_hash[:home][:team_name]
+end
+puts home_team_name
+
+def away_team_name
+  game_hash[:away][:team_name]
+end
+puts away_team_name
+
 binding.pry
+
+
+end
