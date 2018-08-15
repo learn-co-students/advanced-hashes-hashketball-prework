71911973c335f74e2fffaaa85944412a463d2e94
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-15 22:32:48 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -45,7 +45,7 @@ def game_hash
                              blocks: 11,
                              slam_dunks: 1}
                     }
-          }
+          },
     away: {team_name: "Charlotte Hornets",
            colors: ["Turquoise", "Purple"],
            players: {"Jeff Adrien" => { number: 4,
