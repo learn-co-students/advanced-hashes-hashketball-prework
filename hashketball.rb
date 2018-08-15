e1e0dd60e4470c9d0dda0f1e261dc8404ae1cc1f
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-15 22:01:14 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -3,11 +3,92 @@ require "pry"
 def game_hash
   {
     home: {team_name: ""
-           colors: [""]
-           players: {"" => {
+           colors: ["", ""]
+           players: {"" => { number: ,
+                             shoe: , 
+                             points: , 
+                             rebounds: , 
+                             assists: , 
+                             steals: , 
+                             blocks: ,
+                             slam_dunks: },
+                     "" => { number: ,
+                             shoe: , 
+                             points: , 
+                             rebounds: , 
+                             assists: , 
+                             steals: , 
+                             blocks: ,
+                             slam_dunks: },       
+                     "" => { number: ,
+                             shoe: , 
+                             points: , 
+                             rebounds: , 
+                             assists: , 
+                             steals: , 
+                             blocks: ,
+                             slam_dunks: },
+                     "" => { number: ,
+                             shoe: , 
+                             points: , 
+                             rebounds: , 
+                             assists: , 
+                             steals: , 
+                             blocks: ,
+                             slam_dunks: },
+                     "" => { number: ,
+                             shoe: , 
+                             points: , 
+                             rebounds: , 
+                             assists: , 
+                             steals: , 
+                             blocks: ,
+                             slam_dunks: }
+                    }
           }
-    away: {
-          }
+    away: {team_name: ""
+           colors: ["", ""]
+           players: {"" => { number: ,
+                             shoe: , 
+                             points: , 
+                             rebounds: , 
+                             assists: , 
+                             steals: , 
+                             blocks: ,
+                             slam_dunks: },
+                     "" => { number: ,
+                             shoe: , 
+                             points: , 
+                             rebounds: , 
+                             assists: , 
+                             steals: , 
+                             blocks: ,
+                             slam_dunks: },       
+                     "" => { number: ,
+                             shoe: , 
+                             points: , 
+                             rebounds: , 
+                             assists: , 
+                             steals: , 
+                             blocks: ,
+                             slam_dunks: },
+                     "" => { number: ,
+                             shoe: , 
+                             points: , 
+                             rebounds: , 
+                             assists: , 
+                             steals: , 
+                             blocks: ,
+                             slam_dunks: },
+                     "" => { number: ,
+                             shoe: , 
+                             points: , 
+                             rebounds: , 
+                             assists: , 
+                             steals: , 
+                             blocks: ,
+                             slam_dunks: }
+                    }
   }
 end 
 
