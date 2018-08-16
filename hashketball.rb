9ef591af57618689a4cfe67b0228746a0cd9baca
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-16 04:12:21 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -104,13 +104,14 @@ def good_practices
 binding.pry
       team_data.each do |attribute, data|
 #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
+# OG location +> :home
 # OG attribute => :team_name
 # OG team_data returns same as above.
-
+# OG "Brooklyn Nets"
 binding.pry
  
 #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
-#data => "Brooklyn Nets"
+
         data.each do |data_item|
 binding.pry
       end
