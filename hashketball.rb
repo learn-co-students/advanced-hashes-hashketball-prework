be15c010dffb681d111448de13c655d4cd658f76
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-16 22:54:30 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -131,14 +131,15 @@ def team_colors(name_of_team)
   colors_of_team = nil 
   game_hash.each do |location, team_data|
     team_data.each do |attribute, data|
+binding.pry       
       if attribute == :team_name && data = name_of_team
         colors_of_team = team_data[:colors]
-# binding.pry colors_of_team properly returns ["Black, "White]  
+   
       end 
     end
   end
   colors_of_team
-# binding.pry colors_of_team incorrectly returns ["Turquoise", "Purple"], the only other array of colors in the whole hash.
+ 
 end 
 
 # def team_names
