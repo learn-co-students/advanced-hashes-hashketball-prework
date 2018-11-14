d45598e0296f587a83fd862f383475565ae20584
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-14 07:16:26 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,120 @@
 # Write your code here!
-
+def game_hash
+  nba_hash = {
+    :home => {
+      :team_name => "Brooklen Nets",
+      :colors => ["Black","White"],
+      :players => {
+        "Alan Anderson" => {
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
+          }
+        "Reggie Evans" => {
+          :number => 30,
+          :shoe => 14,
+          :points => 12,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 12,
+          :blocks => 12,
+          :slam_dunks => 7
+          } 
+           "Brook Lopez" => {
+          :number => 11,
+          :shoe => 17,
+          :points => 17,
+          :rebounds => 19,
+          :assists => 10,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 15
+          }
+           "Mason Plumlee" => {
+          :number => 1,
+          :shoe => 19,
+          :points => 26,
+          :rebounds => 12,
+          :assists => 6,
+          :steals => 3,
+          :blocks => 8,
+          :slam_dunks => 5
+          }
+           "Jason Terry" => {
+          :number => 31,
+          :shoe => 15,
+          :points => 19,
+          :rebounds => 2,
+          :assists => 2,
+          :steals => 4,
+          :blocks => 11,
+          :slam_dunks => 1
+          } 
+        }
+      }
+    :away => {
+      :team_name => " ",
+      :colors => ["",""],
+      :players => {
+        "Alan Anderson" => {
+          :number => 23,
+          :shoe => 18,
+          :points => 100,
+          :rebounds => 15,
+          :assists => 8,
+          :steals => 11,
+          :blocks => 5,
+          :slam_dunks => 999
+          }
+        "Reggie Evans" => {
+          :number => 23,
+          :shoe => 18,
+          :points => 100,
+          :rebounds => 15,
+          :assists => 8,
+          :steals => 11,
+          :blocks => 5,
+          :slam_dunks => 999
+          } 
+           "Brook Lopez" => {
+          :number => 23,
+          :shoe => 18,
+          :points => 100,
+          :rebounds => 15,
+          :assists => 8,
+          :steals => 11,
+          :blocks => 5,
+          :slam_dunks => 999
+          }
+           "Mason Plumlee" => {
+          :number => 23,
+          :shoe => 18,
+          :points => 100,
+          :rebounds => 15,
+          :assists => 8,
+          :steals => 11,
+          :blocks => 5,
+          :slam_dunks => 999
+          }
+           "Jason Terry" => {
+          :number => 23,
+          :shoe => 18,
+          :points => 100,
+          :rebounds => 15,
+          :assists => 8,
+          :steals => 11,
+          :blocks => 5,
+          :slam_dunks => 999
+          }
+        }
+      }
+  }
+end
 
 
 
