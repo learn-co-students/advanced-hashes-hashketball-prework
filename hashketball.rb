be4bb904df9684ca29f28f480f48b1b3ee0b8552
brinda behal <brindab50@gmail.com>
2019-05-25 03:09:14 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,38 @@
-# Write your code here!
+def game_hash {
+  :home => {
+    :team_name => "Brooklyn Nets"
+    :colors => "Black, White"
+    :players => {
+      :number => 
+      :shoe =>
+      :points =>
+      :rebounds =>
+      :assists =>
+      :steals =>
+      :blocks =>
+      :slam_dunks =>
+    }
+  }
+  :away => {
+    :team_name => "Charlotte Hornets"
+    :colors => "Terquoise, Purple"
+    :players => {
+      :number => 
+      :shoe =>
+      :points =>
+      :rebounds =>
+      :assists =>
+      :steals =>
+      :blocks =>
+      :slam_dunks =>
+    }
+    
+  }
+} 
+  
+  
+  
+end 
 
 
 
