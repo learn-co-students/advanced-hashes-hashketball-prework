df007c85aa965e93c0c9e62b715902fdb578b5c2
Conrad Casper <conradcasper@icloud.com>
2019-05-28 20:26:31 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,242 @@
+
 # Write your code here!
+require "pry"
+
+
+def game_hash
+{
+    :home => {
+  :team_name => "Brooklyn Nets",
+  :colors => ["Black", "White"],
+  :players => {
+    "Alan Anderson" => {
+  :number => 0,
+  :shoe => 16,
+  :points => 22,
+  :rebounds => 12,
+  :assists => 12,
+  :steals => 3,
+  :blocks => 1,
+  :slam_dunks => 1
+},
+    "Reggie Evans" => {
+  :number => 30,
+  :shoe => 14,
+  :points => 12,
+  :rebounds => 12,
+  :assists => 12,
+  :steals => 12,
+  :blocks => 12,
+  :slam_dunks => 7
+},
+    "Brook Lopez" => {
+  :number => 11,
+  :shoe => 17,
+  :points => 17,
+  :rebounds => 19,
+  :assists => 10,
+  :steals => 3,
+  :blocks => 1,
+  :slam_dunks => 15
+},
+    "Mason Plumlee" => {
+  :number => 1,
+  :shoe => 19,
+  :points => 26,
+  :rebounds => 12,
+  :assists => 6,
+  :steals => 3,
+  :blocks => 8,
+  :slam_dunks => 5
+},
+    "Jason Terry" => {
+  :number => 31,
+  :shoe => 15,
+  :points => 19,
+  :rebounds => 2,
+  :assists => 2,
+  :steals => 4,
+  :blocks => 11,
+  :slam_dunks => 1
+}
+}
+},
+    :away => {
+  :team_name => "Charlotte Hornets",
+  :colors => ["Turquoise", "Purple"],
+  :players => {
+    "Jeff Adrien" => {
+  :number => 4,
+  :shoe => 18,
+  :points => 10,
+  :rebounds => 1,
+  :assists => 1,
+  :steals => 2,
+  :blocks => 7,
+  :slam_dunks => 2
+},
+    "Bismak Biyombo" => {
+  :number => 0,
+  :shoe => 16,
+  :points => 12,
+  :rebounds => 4,
+  :assists => 7,
+  :steals => 7,
+  :blocks => 15,
+  :slam_dunks => 10
+},
+    "DeSagna Diop" => {
+  :number => 2,
+  :shoe => 14,
+  :points => 24,
+  :rebounds => 12,
+  :assists => 12,
+  :steals => 4,
+  :blocks => 5,
+  :slam_dunks => 5
+},
+    "Ben Gordon" => {
+  :number => 8,
+  :shoe => 15,
+  :points => 33,
+  :rebounds => 3,
+  :assists => 2,
+  :steals => 1,
+  :blocks => 1,
+  :slam_dunks => 0
+},
+    "Brendan Haywood" => {
+  :number => 33,
+  :shoe => 15,
+  :points => 6,
+  :rebounds => 12,
+  :assists => 12,
+  :steals => 22,
+  :blocks => 5,
+  :slam_dunks => 12
+}
+}
+ 
+  }
+}
+end
+
+
+
+
+
+def num_points_scored(player)
+  hash = game_hash
+  hash.each do |location, team| 
+    team.each do |attribute, data|
+      if attribute == :players
+      data.each do |name, stats|
+        if name == player 
+          # binding.pry
+       return hash[location][attribute][player][:points]
+     end
+      end
+      end
+    end
+  end
+end
+
+
+
+
+def shoe_size(player)
+  hash = game_hash
+  hash.each do |location, team|
+    team.each do |attribute, data|
+      if attribute == :players
+        data.each do |name, stats|
+          if name == player
+            return hash[location][attribute][player][:shoe]
+          end
+        end
+      end
+    end
+  end
+end
+
+
+
+
+
+
+def team_colors(team_name)
+  hash = game_hash
+  hash.each do |location, team|
+    team.each do |attribute, info|
+      # binding.pry
+      if attribute == :team_name && info == team_name
+        return hash[location][:colors]
+      end
+    
+  end
+  end
+end
+
+def team_names
+  hash = game_hash
+  array = Array.new
+  hash.each do |location, team|
+    team.each do |attribute, info|
+      if attribute == :team_name
+        array << info
+      end
+    end
+  end
+  array
+end
+  
+
+
+def player_numbers(team_name)
+  jersey_nums = []
+  game_hash.each do |location, team_data|
+    if team_data.has_value?(team_name)
+      team_data[:players].each do |player, stats|
+          stats.each do |x, y|
+            if x == :number
+              jersey_nums << y
+            end
+      end
+    end
+  end
+end
+jersey_nums
+end
+
+def player_stats(name)
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |player, stats|
+          if player == name
+            return stats
+          end
+        end
+      end
+    end
+    
+    def big_shoe_rebounds
+  shoe_size = 0 
+  rebounds = 0 
+
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |name, stats|
+      if stats[:shoe] > shoe_size
+        shoe_size = stats[:shoe]
+        rebounds = stats[:rebounds]
+      end
+    end
+  end
+  rebounds
+end
+
+
+
+
+
 
 
 
