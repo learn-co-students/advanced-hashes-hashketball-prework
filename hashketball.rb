74d59930c9c2bd0ca4fee165c2d8f0320dd2afc4
github username <github email address>
2019-06-14 23:56:38 -0400

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -2,17 +2,117 @@
 def game_hash
     {
         :home =>{
-            :team_name => 
-            :colors => [],
+            :team_name => "Brooklyn Nets"
+            :colors => ["Black", "White"],
             :players => [
-
+                :Alan_Anderson => {
+                    :number => 0,
+                    :shoe => 16,
+                    :points => 22,
+                    :rebounds => 12,
+                    :assists => 12,
+                    :steals => 3,
+                    :blocks => 1,
+                    :slam_dunks => 1
+                },
+                :Reggie_Evans => {
+                    :number => 30,
+                    :shoe => 14,
+                    :points => 12,
+                    :rebounds => 12,
+                    :assists => 12,
+                    :steals => 12,
+                    :blocks => 12,
+                    :slam_dunks => 1
+                },
+                :Brook_Lopez => {
+                    :number => 11,
+                    :shoe => 17,
+                    :points => 17,
+                    :rebounds => 19,
+                    :assists => 10,
+                    :steals => 3,
+                    :blocks => 1,
+                    :slam_dunks => 15
+                },
+                :Jason_Terry => {
+                    :number => 31,
+                    :shoe => 15,
+                    :points => 19,
+                    :rebounds => 2,
+                    :assists => 2,
+                    :steals => 4,
+                    :blocks => 11,
+                    :slam_dunks => 1
+                },
+                :Mason_Plumlee => {
+                    :number => 1,
+                    :shoe => 19,
+                    :points => 26,
+                    :rebounds => 11,
+                    :assists => 6,
+                    :steals => 3,
+                    :blocks => 8,
+                    :slam_dunks => 5
+                }
             ]
         },
         :away => {
-            :team_name => 
-            :colors => [],
+            :team_name => "Charlotte Hornets"
+            :colors => ["Turquoise", "Purple"],
             :players => [
-
+                {:Jeff_Adrien => {
+                    :number => 
+                    :shoe =>
+                    :points =>
+                    :rebounds => 
+                    :assists => 
+                    :steals =>
+                    :blocks =>
+                    :slam_dunks =>
+                    }
+                },
+                {:Alan_Anderson => {
+                    :number => 
+                    :shoe =>
+                    :points =>
+                    :rebounds => 
+                    :assists => 
+                    :steals =>
+                    :blocks =>
+                    :slam_dunks =>
+                    }
+                },
+                {:Alan_Anderson => {
+                    :number => 
+                    :shoe =>
+                    :points =>
+                    :rebounds => 
+                    :assists => 
+                    :steals =>
+                    :blocks =>
+                    :slam_dunks =>
+                                }                },
+                :Alan_Anderson => {
+                    :number => 
+                    :shoe =>
+                    :points =>
+                    :rebounds => 
+                    :assists => 
+                    :steals =>
+                    :blocks =>
+                    :slam_dunks =>
+                },
+                :Alan_Anderson => {
+                    :number => 
+                    :shoe =>
+                    :points =>
+                    :rebounds => 
+                    :assists => 
+                    :steals =>
+                    :blocks =>
+                    :slam_dunks =>
+                }
             ]
         }
     }
