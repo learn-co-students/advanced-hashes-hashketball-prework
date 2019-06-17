40bb544723692fe725ee8853034683774173c9b8
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-06-17 16:09:30 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -5,24 +5,22 @@ def game_hash => {
     :team_name => "Brooklyn Nets"
     :colors => ["Black", "White"]
     :players => [
-      {name: "Alan Anderson" , number: "0" , shoe: "16", points: "22" , rebounds: "12" , assists: "12" , steals: "3", blocks: "1", slam_dunks: "1"}
-      {name: "Reggie Evans" , number: "30" , shoe: "14" , points: "12" , rebounds: "12" , assists: "12" , steals: "12" , blocks: "12", slam_dunks: "7"}
-      {name: "Brook Lopez" , number: "11" , shoe: "17" , points: "17" , rebounds: "19" , assists: "10" , steals: "3", blocks: "1", slam_dunks: "15"}
-      {name: "Mason Plumee" , number: "1" , shoe: "19" , points: "26" , rebounds: "11", assists: "6" , steals: "3", blocks: "8", slam_dunks: "5"}
-      {name: "Jason Terry" , number: "31" , shoe: "15" , points: "19" , rebounds: "2" , assists: "2" , steals: "4" , blocks: "11", slam_dunks: "1"}
-        
-      
+      :Alan_Anderson => {number: "0", shoe: "16", points: "22", rebounds: "12", assists: "12", steals: "3", blocks: "1", slam_dunks: "1"}
+      :Reggie_Evans => {number: "30", shoe: "14", points: "12", rebounds: "12", assists: "12", steals: "12", blocks: "12", slam_dunks: "7"}
+      :Brook_Lopez => {number: "11", shoe: "17", points: "17", rebounds: "19", assists: "10", steals: "3", blocks: "1", slam_dunks: "15"}
+      :Mason_Plumee => {number: "1", shoe: "19", points: "26", rebounds: "11", assists: "6", steals: "3", blocks: "8", slam_dunks: "5"}
+      :Jason_Terry => {number: "31", shoe: "15", points: "19", rebounds: "2", assists: "2", steals: "4", blocks: "11", slam_dunks: "1"}
       ]
   }
   :away => {
     :team_name => "Charlotte Hornets"
     :colors => ["Turquoise", "Purple"]
     :players => [
-      {name: "Alan Anderson" , number: "0" , shoe: "16", points: "22" , rebounds: "12" , assists: "12" , steals: "3", blocks: "1", slam_dunks: "1" }
-      {name: , number: , shoe: , points: , rebounds: , assists: , steals: , blocks:, slam_dunks:  }
-      {name: , number: , shoe: , points: , rebounds: , assists: , steals: , blocks:, slam_dunks:  }
-      {name: , number: , shoe: , points: , rebounds: , assists: , steals: , blocks:, slam_dunks:  }
-      {name: , number: , shoe: , points: , rebounds: , assists: , steals: , blocks:, slam_dunks:  }
+      :Jeff_Adrien => {number: "4", shoe: "18", points: "10", rebounds: "1", assists: "1", steals: "2", blocks: "7", slam_dunks: "2"}
+      :Bismack_Biyombo => {number: "0", shoe: "16", points: "12", rebounds: "4", assists: "7", steals: "22", blocks: "15", slam_dunks: "10"}
+      {number: , shoe: , points: , rebounds: , assists: , steals: , blocks:, slam_dunks:  }
+      {number: , shoe: , points: , rebounds: , assists: , steals: , blocks:, slam_dunks:  }
+      {number: , shoe: , points: , rebounds: , assists: , steals: , blocks:, slam_dunks:  }
         
       
       ]
