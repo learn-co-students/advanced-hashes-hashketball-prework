da8ae0a8a365fad966c24a7cd7a58a53552c7969
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-06-17 15:00:46 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -3,15 +3,29 @@
 def game_hash => {
   :home => {
     :team_name => "Brooklyn Nets"
-    :colors => [Black, White]
+    :colors => ["Black", "White"]
     :players => [
-      {name: "Alan Anderson" , number: "0" , shoe: "16", points: "22" , rebounds: "12" , assists: "12" , steals: "3", blocks: "1", slam_dunks: "1" }
-      {
+      {name: "Alan Anderson" , number: "0" , shoe: "16", points: "22" , rebounds: "12" , assists: "12" , steals: "3", blocks: "1", slam_dunks: "1"}
+      {name: "Reggie Evans" , number: "30" , shoe: "14" , points: "12" , rebounds: "12" , assists: "12" , steals: "12" , blocks: "12", slam_dunks: "7"}
+      {name: "Brook Lopez" , number: "11" , shoe: "17" , points: "17" , rebounds: "19" , assists: "10" , steals: "3", blocks: "1", slam_dunks: "15"}
+      {name: "Mason Plumee" , number: "1" , shoe: "19" , points: "26" , rebounds: "11", assists: "6" , steals: "3", blocks: "8", slam_dunks: "5" }
+      {name: , number: , shoe: , points: , rebounds: , assists: , steals: , blocks:, slam_dunks:  }
         
-      }
+      
       ]
   }
   :away => {
+    :team_name => "Charlotte Hornets"
+    :colors => ["Turquoise", "Purple"]
+    :players => [
+      {name: "Alan Anderson" , number: "0" , shoe: "16", points: "22" , rebounds: "12" , assists: "12" , steals: "3", blocks: "1", slam_dunks: "1" }
+      {name: , number: , shoe: , points: , rebounds: , assists: , steals: , blocks:, slam_dunks:  }
+      {name: , number: , shoe: , points: , rebounds: , assists: , steals: , blocks:, slam_dunks:  }
+      {name: , number: , shoe: , points: , rebounds: , assists: , steals: , blocks:, slam_dunks:  }
+      {name: , number: , shoe: , points: , rebounds: , assists: , steals: , blocks:, slam_dunks:  }
+        
+      
+      ]
     
   }
 }
